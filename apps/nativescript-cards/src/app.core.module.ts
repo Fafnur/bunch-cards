9c2660f338b5b2b0bc1e8503f853d5aed5c30ba0
Aleksandr Serenko <fafnur@yandex.ru>
2022-04-12 09:43:16 +0700

    [Core][Store] Adding root store

--- apps/nativescript-cards/src/app.core.module.ts
+++ apps/nativescript-cards/src/app.core.module.ts
@@ -3,13 +3,22 @@ import {
   NativeScriptHttpClientModule,
   NativeScriptModule,
 } from '@nativescript/angular';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { ENVIRONMENTS } from '@bunch/core/environments';
+import { RootStoreModule } from '@bunch/core/store/root';
 
 import { environment } from './environments/environment';
 
 @NgModule({
-  imports: [NativeScriptModule, NativeScriptHttpClientModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptHttpClientModule,
+    RootStoreModule,
+    !environment.production
+      ? StoreDevtoolsModule.instrument({ logOnly: environment.production })
+      : [],
+  ],
   providers: [
     {
       provide: ENVIRONMENTS,
--- apps/nativescript-cards/src/app.module.ts
+++ apps/nativescript-cards/src/app.module.ts
@@ -1,32 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { AppCoreModule } from './app.core.module';
 import { AppRoutingModule } from './app.routing';
-import { environment } from './environments/environment';
 import { SharedModule } from './features/shared/shared.module';
 
 @NgModule({
-  imports: [
-    AppCoreModule,
-    SharedModule,
-    AppRoutingModule,
-    StoreModule.forRoot(
-      {},
-      {
-        metaReducers: !environment.production ? [] : [],
-        runtimeChecks: {
-          strictActionImmutability: true,
-          strictStateImmutability: true,
-        },
-      }
-    ),
-    EffectsModule.forRoot([]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-  ],
+  imports: [AppCoreModule, SharedModule, AppRoutingModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
