caa0f706b20f6465261aa9bb101481160f708b13
Aleksandr Serenko <fafnur@yandex.ru>
2022-04-13 23:07:32 +0700

    [Web][Ui] Adding theming

---
+++ libs/web/ui/theming/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/image/background-image.pipe';
+export * from './lib/image/image.module';
+export * from './lib/link/link.directive';
+export * from './lib/link/link.module';
+export * from './lib/width/full-width.directive';
+export * from './lib/width/width.module';
---
+++ libs/web/ui/theming/src/lib/width/full-width.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[bunchFullWidth],[bunch-full-width]',
+})
+export class FullWidthDirective {}
