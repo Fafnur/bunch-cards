2f9e80bc96cfbed7991fb34bc90987e01e3cfc63
Aleksandr Serenko <fafnur@yandex.ru>
2022-04-14 00:13:49 +0700

    [Web][App] Include layout for web app

--- apps/web-cards/src/app/app.routing.module.ts
+++ apps/web-cards/src/app/app.routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from '@bunch/web/ui/layout';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [],
+  },
+];
 
 @NgModule({
   imports: [
