fdc7ffba6a6ea54215b42fc230886f5450a8fbb4
Aleksandr Serenko <fafnur@yandex.ru>
2022-08-09 22:04:22 +0700

    [Users][Bug] Fixing users imports for state

--- libs/users/state/src/lib/user-state.module.ts
+++ libs/users/state/src/lib/user-state.module.ts
@@ -1,14 +1,15 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+
+import { UserManagerModule } from '@bunch/users/manager';
 
 import { UserEffects } from './user.effects';
 import { UserFacade } from './user.facade';
 import { USER_FEATURE_KEY, userReducer } from './user.reducer';
 
 @NgModule({
-  imports: [CommonModule, StoreModule.forFeature(USER_FEATURE_KEY, userReducer), EffectsModule.forFeature([UserEffects])],
+  imports: [UserManagerModule, StoreModule.forFeature(USER_FEATURE_KEY, userReducer), EffectsModule.forFeature([UserEffects])],
   providers: [UserFacade],
 })
 export class UserStateModule {}
