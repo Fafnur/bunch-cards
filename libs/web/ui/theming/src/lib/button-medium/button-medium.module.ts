ad5715b9941fe1ac8fb31a3d591d5f7d491d6fcf
Aleksandr Serenko <fafnur@yandex.ru>
2022-08-11 17:37:47 +0700

    [Web][Theming] Adding button medium directive

---
+++ libs/web/ui/theming/src/lib/button-medium/button-medium.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { ButtonMediumDirective } from './button-medium.directive';
+
+@NgModule({
+  declarations: [ButtonMediumDirective],
+  exports: [ButtonMediumDirective],
+})
+export class ButtonMediumModule {}
