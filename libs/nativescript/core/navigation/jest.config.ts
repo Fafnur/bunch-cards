ed4c818a3507758d715903c04fbdff2633ed51de
Aleksandr Serenko <fafnur@yandex.ru>
2022-08-19 08:41:02 +0700

    [Core][Navigation] Adding ns navigation service

---
+++ libs/nativescript/core/navigation/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'nativescript-core-navigation',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/nativescript/core/navigation',
+};
