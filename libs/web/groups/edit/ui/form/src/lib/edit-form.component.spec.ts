1596df63a7c39420cced40487d3164c29a1b8fcc
Aleksandr Serenko <fafnur@yandex.ru>
2022-09-07 08:04:32 +0700

    [Web][Groups][UI] Fix tests

--- libs/web/groups/edit/ui/form/src/lib/edit-form.component.spec.ts
+++ libs/web/groups/edit/ui/form/src/lib/edit-form.component.spec.ts
@@ -5,9 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MockModule } from 'ng-mocks';
-import { mock } from 'ts-mockito';
+import { ReplaySubject } from 'rxjs';
+import { mock, when } from 'ts-mockito';
 
 import { providerOf } from '@bunch/core/testing';
+import { Group, GROUP_STUB } from '@bunch/groups/common';
 import { GroupFacade } from '@bunch/groups/state';
 import { WidthModule } from '@bunch/web/ui/theming';
 
@@ -17,9 +19,17 @@ describe('EditFormComponent', () => {
   let component: EditFormComponent;
   let fixture: ComponentFixture<EditFormComponent>;
   let groupFacadeMock: GroupFacade;
+  let changeFailure$: ReplaySubject<unknown>;
+  let changeSuccess$: ReplaySubject<Group>;
 
   beforeEach(async () => {
     groupFacadeMock = mock(GroupFacade);
+
+    changeFailure$ = new ReplaySubject<unknown>(1);
+    changeSuccess$ = new ReplaySubject<Group>(1);
+
+    when(groupFacadeMock.changeFailure$(GROUP_STUB.uuid)).thenReturn(changeFailure$);
+    when(groupFacadeMock.changeSuccess$(GROUP_STUB.uuid)).thenReturn(changeSuccess$);
 
     await TestBed.configureTestingModule({
       imports: [
@@ -36,6 +46,8 @@ describe('EditFormComponent', () => {
 
     fixture = TestBed.createComponent(EditFormComponent);
     component = fixture.componentInstance;
+    // TODO: Make WrapperComponent
+    component.group = GROUP_STUB;
   });
 
   it('should create', () => {
--- libs/web/groups/ui/card/src/lib/group-card.component.spec.ts
+++ libs/web/groups/ui/card/src/lib/group-card.component.spec.ts
@@ -5,7 +5,10 @@ import { MatDividerModule } from '@angular/material/divider';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockModule } from 'ng-mocks';
+import { mock, when } from 'ts-mockito';
 
+import { NAVIGATION_PATHS, NavigationService, NavigationServiceStub } from '@bunch/core/navigation';
+import { providerOf } from '@bunch/core/testing';
 import { GROUP_STUB } from '@bunch/groups/common';
 
 import { GroupCardComponent } from './group-card.component';
@@ -13,11 +16,17 @@ import { GroupCardComponent } from './group-card.component';
 describe('GroupCardComponent', () => {
   let component: GroupCardComponent;
   let fixture: ComponentFixture<GroupCardComponent>;
+  let navigationServiceMock: NavigationService;
 
   beforeEach(async () => {
+    navigationServiceMock = mock(NavigationServiceStub);
+
+    when(navigationServiceMock.getPaths()).thenReturn(NAVIGATION_PATHS);
+
     await TestBed.configureTestingModule({
       imports: [CommonModule, NoopAnimationsModule, RouterTestingModule, MockModule(MatCardModule), MockModule(MatDividerModule)],
       declarations: [GroupCardComponent],
+      providers: [providerOf(NavigationService, navigationServiceMock)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GroupCardComponent);
